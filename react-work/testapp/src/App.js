664e54bba7470bb194d248a7989c3787625cc7aa
mean <meanstack4@gmail.com>
2025-02-10 11:08:09 +0530

    React introduction

---
+++ react-work/testapp/src/App.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class App extends Component{
+  render(){
+    return <>
+      <h1 className="" style={{color:"green"}}>Welcome In Reactjs Application</h1>
+    </>
+  }
+}
+export default App;
